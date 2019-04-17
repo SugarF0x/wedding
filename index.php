1c4626044143ae79c92252b5f47f38809a5c061d
SugarF0x <malion.torchwood@yandex.ru>
2019-04-17 17:00:11 +0300

    0.1
    Initial bootstrap setup for the landing page
    Have literally no idea what im gonna do with that, i have zero to no experience with bootstrap, but it aint ever late to learn

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Приглашение на свадьбу</title>
+</head>
+<body>
+<!-- -->
+
+<div class="container">
+    <h1>Testing bootstrap</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis iste minima molestias obcaecati, qui quia quidem quod sit tenetur veritatis.</p>
+</div>
+
+<!-- -->
+</body>
+</html>
