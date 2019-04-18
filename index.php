5c275489a26eabede05451d0338b12a9b2a68505
SugarF0x <malion.torchwood@yandex.ru>
2019-04-18 13:42:58 +0300

    0.3
    Fucking finally managed to center the date and shit for both desktop and phone, ffs this was horrible

--- index.php
+++ index.php
@@ -7,13 +7,86 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
 
-    <title>Hello, world!</title>
+    <title>Save the Date</title>
 </head>
 <body>
-<!-- Body Start -->
+<!-- Body Start justify-content-center align-items-center  -->
 
-<h1>Hello, world!</h1>
+<div class="container d-flex align-items-center justify-content-center h-100">
+    <!--
+    <div class="text-center">
+        <h3>Save date card</h3>
+        <p>
+            This is a save date card with a lot of sample text <br>
+            Lorem ipsum dolor sit amet, consectetur <br>
+            adipisicing elit. Accusamus blanditiis excepturi <br>
+            expedita incidunt magnam magni minus perspiciatis quidem?
+        </p>
+        <ul style="list-style: none; padding: 0;">
+            <li>Start date: <b>01.10.1939</b></li>
+            <li>Start place: <b>Polen</b></li>
+        </ul>
+        <p>
+            Contact us at <b>8 (800) 555-35-35</b> <br>
+            Easier to call than take a loan
+        </p>
+    </div>
+    -->
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <h1>
+                    Save the date
+                </h1>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center">
+            <div class="col-4 col-md-auto text-right">
+                <h5>
+                    July
+                </h5>
+            </div>
+            <div class="col-auto" style="border-left: 1px solid black; border-right: 1px solid black;">
+                <h3>
+                    16
+                </h3>
+            </div>
+            <div class="col-4 col-md-auto text-left">
+                <h5>
+                    2019
+                </h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                For the wedding of
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center">
+            <div class="col-5 col-md-3 col-lg-2 text-right">
+                <h3>
+                    Yaroslav
+                </h3>
+            </div>
+            <div class="col-auto">
+                and
+            </div>
+            <div class="col-5 col-md-3 col-lg-2 text-left">
+                <h3>
+                    Maria
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Contact at: 555 35 35
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
